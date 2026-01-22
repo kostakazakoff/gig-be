872bf551415d9bb41e2601cfdacc6988eae89b44
Kosta <kostakazakoff@gmail.com>
2026-01-22 04:00:30 +0200

    refactor: Enhance admin message handling for existing and new clients in StoreApiInquiry

--- app/AppServices/Inquiry/StoreApiInquiry.php
+++ app/AppServices/Inquiry/StoreApiInquiry.php
@@ -14,31 +14,33 @@ class StoreApiInquiry
         public CheckExistingClient $checkExistingClient,
         public StoreClient $storeClient,
         private $language = 'bg',
-    )
-    {
+        private $adminLanguage = 'bg',
+        private $adminMessage = '',
+    ) {
         $this->language = config('app.locale', $language);
     }
-    
+
     public function handle(StoreInquiryRequest $request): array
     {
         $client = $this->checkExistingClient->handle($request->email);
         $isNewClient = !$client;
 
-        if (!$isNewClient) {
-            $this->language = $client->language;
-        }
-
-        if ($isNewClient) {
-            $client = $this->storeClient->handle($request);
+        switch ($isNewClient) {
+            case true:
+                $this->language = $request->language ?? config('app.locale', 'bg');
+                $client = $this->storeClient->handle($request);
+                $this->adminMessage = __('messages.admin_new_client_message', [], $this->adminLanguage);
+                break;
+            case false:
+                $this->language = $client->language;
+                $this->adminMessage = __('messages.admin_existing_client_message', [], $this->adminLanguage);
+                break;
         }
 
         $message = __('messages.client_success_message', [], $this->language);
 
-        // TODO: fix admin language to be dynamic when Admin Panel with language settings is implemented
-        $adminMessage = ($isNewClient ? __('messages.admin_new_client_message', [], 'bg') : __('messages.admin_existing_client_message', [], 'bg'));
-        
         $successMessage = __('messages.success_response', [], $this->language);
-        
+
         $inquiry = Inquiry::create([
             'client_id' => $client->id,
             'message' => $request->message,
@@ -49,7 +51,7 @@ class StoreApiInquiry
         SendEmail::dispatch($client->email, $message, $this->language, 'client');
 
         // TODO: fix admin language to be dynamic when Admin Panel with language settings is implemented
-        SendEmail::dispatch(config('mail.admin_email'), $adminMessage, 'bg', 'admin');
+        SendEmail::dispatch(config('mail.admin_email'), $this->adminMessage, $this->adminLanguage, 'admin');
 
         return [
             'inquiry' => $inquiry,
